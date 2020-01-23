82b00ff0550ca881c72fda2ae3249ec83b2e69df
Marc R. Hoffmann <hoffmann@mountainminds.com>
2020-01-23 16:24:57 +0100

    Add health check

--- src/main/java/io/javaalmanac/sandbox/service/Server.java
+++ src/main/java/io/javaalmanac/sandbox/service/Server.java
@@ -6,6 +6,7 @@ public class Server {
 
 	public static void main(String[] args) {
 		Javalin app = Javalin.create().start(80);
+		app.get("/health", ctx -> ctx.result("ok"));
 		app.post("/compileandrun", new CompileAndRunHandler());
 	}
 
