9e019a865a732999776b662061c2493be2534344
Marc R. Hoffmann <hoffmann@mountainminds.com>
2020-01-23 16:00:38 +0100

    Run on port 80

--- src/main/java/io/javaalmanac/sandbox/service/Server.java
+++ src/main/java/io/javaalmanac/sandbox/service/Server.java
@@ -5,7 +5,7 @@ import io.javalin.Javalin;
 public class Server {
 
 	public static void main(String[] args) {
-		Javalin app = Javalin.create().start(8000);
+		Javalin app = Javalin.create().start(80);
 		app.post("/compileandrun", new CompileAndRunHandler());
 	}
 
