72f318122c804fc7be0092d7674c822fdc08a9e3
Marc R. Hoffmann <hoffmann@mountainminds.com>
2020-02-09 23:11:22 +0100

    Make port configurable

--- src/main/java/io/javaalmanac/sandbox/service/Server.java
+++ src/main/java/io/javaalmanac/sandbox/service/Server.java
@@ -5,7 +5,8 @@ import io.javalin.Javalin;
 public class Server {
 
 	public static void main(String[] args) {
-		Javalin app = Javalin.create().start(80);
+		int port = Integer.parseInt(System.getenv("PORT"));
+		Javalin app = Javalin.create().start(port);
 		app.get("/health", ctx -> ctx.result("ok"));
 		app.get("/version", ctx -> ctx.result(System.getProperty("java.version")));
 		app.post("/compileandrun", new CompileAndRunHandler());
