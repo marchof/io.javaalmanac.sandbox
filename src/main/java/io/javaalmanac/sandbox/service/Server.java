998c01580d30dc08dd3847f365315973cad9e3ed
Marc R. Hoffmann <hoffmann@mountainminds.com>
2020-02-09 21:52:42 +0100

    Add Java version info

--- src/main/java/io/javaalmanac/sandbox/service/Server.java
+++ src/main/java/io/javaalmanac/sandbox/service/Server.java
@@ -7,6 +7,7 @@ public class Server {
 	public static void main(String[] args) {
 		Javalin app = Javalin.create().start(80);
 		app.get("/health", ctx -> ctx.result("ok"));
+		app.get("/version", ctx -> ctx.result(System.getProperty("java.version")));
 		app.post("/compileandrun", new CompileAndRunHandler());
 	}
 
