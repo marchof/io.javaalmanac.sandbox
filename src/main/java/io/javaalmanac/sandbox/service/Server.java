44862a077a72c5d5a7684b014d2c033388767478
Marc R. Hoffmann <hoffmann@mountainminds.com>
2020-02-11 17:17:12 +0100

    Add CORS header

--- src/main/java/io/javaalmanac/sandbox/service/Server.java
+++ src/main/java/io/javaalmanac/sandbox/service/Server.java
@@ -6,7 +6,9 @@ public class Server {
 
 	public static void main(String[] args) {
 		int port = Integer.parseInt(System.getenv("PORT"));
-		Javalin app = Javalin.create().start(port);
+		Javalin app = Javalin.create(config -> {
+			config.enableCorsForOrigin("http://localhost", "https://javaalmanac.io/", "https://www.javaalmanac.io/");
+		}).start(port);
 		app.get("/health", ctx -> ctx.result("ok"));
 		app.get("/version", ctx -> ctx.result(System.getProperty("java.version")));
 		app.post("/compileandrun", new CompileAndRunHandler());
