952877ba6d01ca885ce3e348212a4fc12adf1b7f
Marc R. Hoffmann <hoffmann@mountainminds.com>
2020-05-14 22:39:42 +0200

    Add simple access logging

--- src/main/java/io/javaalmanac/sandbox/service/Server.java
+++ src/main/java/io/javaalmanac/sandbox/service/Server.java
@@ -1,13 +1,22 @@
 package io.javaalmanac.sandbox.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.javalin.Javalin;
 
 public class Server {
+
+	private static final Logger LOG = LoggerFactory.getLogger("access");
 
 	public static void main(String[] args) {
 		int port = Integer.parseInt(System.getenv("PORT"));
 		Javalin app = Javalin.create(config -> {
 			config.enableCorsForOrigin("http://localhost", "https://javaalmanac.io/", "https://www.javaalmanac.io/");
+			config.requestLogger((ctx, ms) -> {
+				LOG.info("{} \"{} {} {}\" {} in {}ms", //
+						ctx.ip(), ctx.method(), ctx.path(), ctx.protocol(), ctx.res.getStatus(), ms);
+			});
 		}).start(port);
 		app.get("/health", ctx -> ctx.result("ok"));
 		app.get("/version", ctx -> ctx.result(System.getProperty("java.vm.version")));
